a0fefcfc8f112e6785571f34495cf436e35794ce
Faris Masri <Faris@macbook-pro.myfiosgateway.com>
2022-04-22 01:14:16 -0400

    all done

--- controllers/guestsController.js
+++ controllers/guestsController.js
@@ -4,6 +4,11 @@ const nodemailer = require("nodemailer");
 let handlebars = require("handlebars");
 let fs = require("fs");
 let path = require("path");
+var AWS = require("aws-sdk"),
+  region = "us-east-1",
+  secretName = "wedding/emailCreds",
+  secret,
+  decodedBinarySecret;
 
 const Guest = require("../models/guest");
 
@@ -77,23 +82,63 @@ async function sendEmail(email, subject, replacements) {
   const source = fs.readFileSync(filePath, "utf-8").toString();
   const template = handlebars.compile(source);
   const htmlToSend = template(replacements);
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: client.user,
-      pass: client.pass,
-    },
+
+  var client = new AWS.SecretsManager({
+    region: region,
   });
-  const mailOptions = {
-    from: "farisanddahlia@gmail.com",
-    to: email,
-    subject: subject,
-    text: "text",
-    html: htmlToSend,
-  };
-  const info = await transporter.sendMail(mailOptions);
-  console.log("Message sent: %s", info.messageId);
-  console.log("Preview URL: %s", "https://mailtrap.io/inboxes/test/messages/");
+
+  client.getSecretValue({ SecretId: secretName }, function (err, data) {
+    if (err) {
+      if (err.code === "DecryptionFailureException")
+        // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        throw err;
+      else if (err.code === "InternalServiceErrorException")
+        // An error occurred on the server side.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        throw err;
+      else if (err.code === "InvalidParameterException")
+        // You provided an invalid value for a parameter.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        throw err;
+      else if (err.code === "InvalidRequestException")
+        // You provided a parameter value that is not valid for the current state of the resource.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        throw err;
+      else if (err.code === "ResourceNotFoundException")
+        // We can't find the resource that you asked for.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        throw err;
+    } else {
+      // Decrypts secret using the associated KMS key.
+      // Depending on whether the secret is a string or binary, one of these fields will be populated.
+      if ("SecretString" in data) {
+        secret = data.SecretString;
+      } else {
+        let buff = new Buffer(data.SecretBinary, "base64");
+        decodedBinarySecret = buff.toString("ascii");
+      }
+    }
+    const secretJSON = JSON.parse(secret);
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: secretJSON.user,
+        pass: secretJSON.pass,
+      },
+    });
+
+    const mailOptions = {
+      from: "farisanddahlia@gmail.com",
+      to: email,
+      subject: subject,
+      text: "text",
+      html: htmlToSend,
+    };
+    const info = transporter.sendMail(mailOptions);
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", "https://mailtrap.io/inboxes/test/messages/");
+  });
 }
 
 module.exports = router;
